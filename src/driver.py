57a9ce44e41a244c94eaa69e1d7de6ee6ec1de51
SMcStay <scottjmcstay@gmail.com>
2024-02-18 00:45:01 -0500

    commit

--- src/driver.py
+++ src/driver.py
@@ -14,15 +14,25 @@ class Driver:
         #for i in range(len(C.hall_links)):
            temp = Dining_Hall_File(C.hall_links[0][0],C.hall_links[0][1])
            txt = Pdf_To_Txt(temp.get_hall_file())
+
+
     def person_setup(self):
         age = input("please enter your age ")
         weight = input("please enter your weight in lbs ")
         height = input("please enter your height in inches ")
         sex = input("please enter your sex, male or female ")
-        person = Person(age,weight,height,sex)
+        exercise = int(input("please enter your exercise level on a scale of 1-10"))-5
+        person = Person(age,weight,height,sex, exercise)
         person.bmi_calc()
-        print(person.bmi)
-        print(person.weight)
+        person.calories()
+        person.nutrients()
+        print(person.carbs_g)
+        print(person.fats_g)
+        print(person.fiber_g)
+        print(person.protein_g)
+
+
+    
 
 
            
--- src/person.py
+++ src/person.py
@@ -1,10 +1,11 @@
 from src.constants import Constants as C
 class Person:
-    def __init__(self,age,weight,height,sex):
+    def __init__(self,age,weight,height,sex,exercise):
         self.age = int(age)
         self.weight = int(weight)
         self.height = int(height)
         self.sex = sex
+        self.exercise = exercise
 
 
     def bmi_calc(self):
@@ -24,23 +25,29 @@ class Person:
             if C.BMI_RANGES[i][1] <= self.bmi <= C.BMI_RANGES[i][2]:
                 self.weight_status = C.BMI_RANGES[i][0]
                 return self.bmi ,self.weight_status
-            
+
+    def calories(self):
+        self.daily_cal = (4.5*self.weight)+(self.height*15.6) -(5*self.age)
+        if self.sex == "male":
+            self.daily_cal += 5
+        else:
+            self.daily_cal -= 161
+        if self.exercise != 0:
+            self.daily_cal += 100 * self.exercise
+
+    def macro_nut(self,low,high,cal_per_g):
+        low_num = int(float(self.daily_cal) * low /cal_per_g)
+        high_num = int(float(self.daily_cal) * high / cal_per_g)
+        return low_num,high_num
+
+    
+    def nutrients(self):
+        self.carbs_g = self.macro_nut(.45,.65,4)
+        self.fats_g = self.macro_nut(.20,.35,9)
+        self.fiber_g = int(self.daily_cal / 1000 *14)
+        self.protein_g = int(self.weight *.5)
             
 
         
 
-        
 
-
-
-
-
-
-
-
-
-    def print(self):
-        print(self.age)
-        print(self.weight)
-        print(self.height)
-        print(self.sex)
