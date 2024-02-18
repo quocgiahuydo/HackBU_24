ae3dbb101d16c092c288b0d8c4c607370bcb321d
h1tenM <mhiten05@gmail.com>
2024-02-18 01:23:35 -0500

    lol

--- src/data_config/pdf_to_txt.py
+++ src/data_config/pdf_to_txt.py
@@ -1,8 +1,9 @@
 import convertapi
 from src.food import Food
+from src.api_key import API
 
 class Pdf_to_Txt(self,file):
-    convertapi.api_secret = 'mP6YytAVdPNN76aK'
+    convertapi.api_secret = f'{API.pdf_2_text}'
     convertapi.convert('txt', {
         'File': file
     }, from_format = 'pdf').save_files('src.data_config')
