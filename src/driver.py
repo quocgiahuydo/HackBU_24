6e2aef96ce0f1626c9b624cde40ab5f4613a5398
h1tenM <mhiten05@gmail.com>
2024-02-18 10:54:32 -0500

    lol

--- src/driver.py
+++ src/driver.py
@@ -4,6 +4,7 @@ from src.data_config.dining_hall_file import Dining_Hall_File
 from src.data_config.nutrition import Nutrition
 from src.data_config.pdf_to_txt import Pdf_To_Txt
 from src.data_config.txt_process import Txt_Process
+from src.meal_options import Meals
 
 class Driver:
     def __init__ (self):
@@ -13,7 +14,7 @@ class Driver:
     def config_data(self):
         #for i in range(len(C.hall_links)):
            temp = Dining_Hall_File(C.hall_links[0][0],C.hall_links[0][1])
-           txt = Pdf_To_Txt(temp.get_hall_file())
+           Pdf_To_Txt(temp.get_hall_file())
 
 
     def person_setup(self):
@@ -22,14 +23,14 @@ class Driver:
         height = input("please enter your height in inches ")
         sex = input("please enter your sex, male or female ")
         exercise = int(input("please enter your exercise level on a scale of 1-10"))-5
-        person = Person(age,weight,height,sex, exercise)
-        person.bmi_calc()
-        person.calories()
-        person.nutrients()
-        print(person.carbs_g)
-        print(person.fats_g)
-        print(person.fiber_g)
-        print(person.protein_g)
+        self.person = Person(age,weight,height,sex, exercise)
+        self.person.bmi_calc()
+        self.person.calories()
+        self.person.nutrients()
+        print(self.person.carbs_g)
+        print(self.person.fats_g)
+        print(self.person.fiber_g)
+        print(self.person.protein_g)
 
 
     
@@ -39,10 +40,17 @@ class Driver:
 
     def run(self):
         #self.config_data()
-        #self.person_setup()
+        self.person_setup()
         x = Txt_Process("c4.txt","c4_output.txt")
         food = Nutrition("c4_output.txt")
         food.values()
+        meal1 = Meals(self.person.daily_cal,self.person.protein_g,food.cal_pro_list)
+        meal1.meal_combos()
+        meal2 = Meals(self.person.daily_cal,self.person.protein_g,food.cal_pro_list)
+        meal3 = Meals(self.person.daily_cal,self.person.protein_g,food.cal_pro_list)
+        print(f"{meal1.meal_items}this set of meal has daily cals {meal1.cal_sum} and total protien {meal1.pro_sum} grams")
+        
+        
        
         
 
---
+++ src/meal_options.py
@@ -0,0 +1,42 @@
+from ortools.sat.python import cp_model
+import numpy as np
+from src.constants import Constants as C
+
+class Meals:
+    def __init__(self,cal,pro,nutri):
+        self.cal = cal
+        self.pro = pro
+        self.nutri = nutri
+    def meal_combos(self):
+        CAL_TOLERANCE = self.cal/20
+        self.cal_sum = 0
+        self.pro_sum = 0
+        self.meal_items = []
+        repeat =0
+        repeated_meals = []
+        P_TOLERANCE = self.pro/20
+        
+        while not (self.cal - CAL_TOLERANCE < self.cal_sum < self.cal + CAL_TOLERANCE) and not (self.pro - P_TOLERANCE < self.pro_sum < self.pro + P_TOLERANCE):
+            temp = np.random.randint(len(self.nutri))
+            if self.nutri[temp][0] in self.meal_items:  
+               repeat += 1 
+               
+            self.meal_items.append(self.nutri[temp][0])
+            self.cal_sum += self.nutri[temp][1]
+            self.pro_sum += self.nutri[temp][2]
+            print(1)
+        for i in self.meal_items:
+            if self.meal_items.count(i) > 1:
+                repeated_meals.append(f"{self.meal_items.count(i)} {i}")
+                print(2)
+                self.meal_items = [meals for meals in self.meal_items if meals != i]    
+            self.meal_items += repeated_meals
+            print(3)
+            
+            
+            
+
+
+
+
+
