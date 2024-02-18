3fa7bee46e3d29acac1aec3823256121787e3493
SMcStay <scottjmcstay@gmail.com>
2024-02-17 19:17:25 -0500

    pull

--- food.py
+++ food.py
@@ -8,7 +8,7 @@ import os
 
 def get_c4_menu():
     current_day = date.today().strftime('%A')
-    folder_path = '/Users/hitenmalhotra/documents/github/hackbu24/food_menus'
+    folder_path = 'food_menus/'
     pdf_path = os.path.join(folder_path, 'c4.pdf')
     pdf_link = None
     soup = BeautifulSoup(requests.get('https://binghamton.sodexomyway.com/dining-near-me/c4-dining-hall').text, "html.parser")
